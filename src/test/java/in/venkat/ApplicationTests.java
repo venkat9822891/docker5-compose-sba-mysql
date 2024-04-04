946d87fbd94a8caec7b5ca7ef712b1b4f765561d
lohit <soaadmn@gmail.com>
2024-04-04 16:21:52 +0530

    Modified

---
+++ src/test/java/in/venkat/ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.venkat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
